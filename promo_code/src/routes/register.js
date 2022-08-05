2a2fa32cfaf4e2bfd708353a0b710ecde7312c52
unknown <janakiraman@infoskies.com>
2022-08-06 00:43:12 +0530

    updating-coupon-design

--- promo_code/src/routes/register.js
+++ promo_code/src/routes/register.js
@@ -1,26 +1,29 @@
 const express= require('express')
 const router= new express.Router()
 const app= express()
-const registerModal= require('../modal/register')
+const registerModal= require('../modal/registerModal')
 require('../db/mongoose')
-
 app.use(express.json())
 
 
-router.post('/register' , async(req , res)=>{
+router.post('/user' , async(req , res)=>{
 try {
     const user = new registerModal(req.body)
     if(!user) return res.status(400).send("Given data is invalid")
     
+    // const {token , decrypt}= await user.generateJWT() 
+    // console.log(req.header('role'))
+    // // console.log(token ,decrypt , )
     if(req.query.referrer){
         let findingOne= await registerModal.findOne({referralLink:req.query.referrer})
         if(!findingOne) {
             await user.save()
             return res.status(201).send("Referral count added")
         }
-        
+        console.log('hjavvfushv')
         user.wallet=user.wallet+20
-        await user.save()            
+        await user.save()         
+
 
     const detailsObj={referredPersonId:user.id ,referredPersonUsername:user.username }
        let updatingOne= await registerModal.updateOne({referralLink:req.query.referrer},
--- promo_code/src/server.js
+++ promo_code/src/server.js
@@ -5,9 +5,16 @@ app.use(express.json())
 require('dotenv/config')
 require('./db/mongoose')
 const registerRoute= require('./routes/register')
-
+const adminCouponCreationRoute= require('./routes/adminCouponCreation')
+const couponListRoute = require('./routes/couponList')
+const updatingCouponDetails= require('./routes/updatingCouponDetails')
+const deletingCoupon= require('./routes/deletingCoupon')
 
 app.use(registerRoute)
+app.use(adminCouponCreationRoute)
+app.use(couponListRoute)
+app.use(updatingCouponDetails)
+app.use(deletingCoupon)
 
 
 app.listen(port ,()=>{
